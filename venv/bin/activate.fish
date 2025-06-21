ebc9c144c29c0bf7fa0d2f347c3fa2a36181026c
tunguyenn99 <tunguyen150599@gmail.com>
2025-06-22 01:04:11 +0700

    commit

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /home/tunguyenn99/project/dbt-databricks/venv
+set -gx VIRTUAL_ENV /home/tunguyenn99/project-vscode/dbt-databricks/venv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
